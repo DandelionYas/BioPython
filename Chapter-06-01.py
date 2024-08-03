92293b4f965645b85598aac999b47c03933f7592
Yaser <yaser.ghaderipour@gmail.com>
2024-08-03 08:41:18 -0400

    Chapter 06: Data types, Create String, Check some string functions

---
+++ Chapter-06-01.py
@@ -0,0 +1,32 @@
+#Datatypes
+print(type(5))
+print(type(5.0))
+print(type(False))
+print(type('ATGATG'))
+
+#Create String
+my_str="This is a string in python"
+my_str='This is a string in python'
+multi_line='''This is fist line,
+This is second line,
+I'm very excited to learn python'''
+print(multi_line)
+multi_line='This is fist line,\nThis is second line,\nI\'m very excited to learn python'
+print(multi_line)
+my_str = 'C:\\new files'
+print(my_str)
+my_str='Hello\tPythonists'
+print(my_str)
+my_str='I\'m a \"MOLCOLAR\" biologist'
+
+#Play with the String functions
+print(my_str)
+name='yaser ghaderipour'
+print(name.title())
+print(name.upper())
+name='YASER'
+print(name.lower())
+print(name)
+txt='I am a coder!'
+txt2= txt.replace('coder', 'Pythonist')
+print(txt2)
