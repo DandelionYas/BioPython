8e60a38b629dd534f4413e106829d425f5f2dcba
Yaser <yaser.ghaderipour@gmail.com>
2024-08-03 08:38:41 -0400

    Chapter 05: First Python code

---
+++ Chapter-05.py
@@ -0,0 +1,8 @@
+print('Hello world!!')
+dna_1="ATG-"*3
+dna_2="CGG-"*3
+full_dna=dna_1+dna_2
+print(full_dna)
+#As a reason for motivation solve the following challenge
+#Challenge: How to remove last '-' from the output?
+print(full_dna[:-1])
