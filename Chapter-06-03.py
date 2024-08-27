0d50f5d62ea8e3920d79ca430863949bb518507a
Yaser <yaser.ghaderipour@gmail.com>
2024-08-27 05:25:49 -0400

    Chapter 06: Special arithmetic operators, comparison, type convention

---
+++ Chapter-06-03.py
@@ -0,0 +1,25 @@
+# Special arithmetic operators
+a=5
+b=2
+print('Inital values: a=', a, ' b=', b)
+print('User / for Divistion: ',a/b)
+print('Use // for Integer Divistion: ',a//b)
+print('Use ** for ^ in Python: ', a**b)
+
+# Special comparison in Python
+print('Is a equals to b?', a == b)
+print('type (a == b)', type(a == b))
+print('Hello,'+'Hello')
+print('Hello,'*10)
+print('Navid'=='Navid')
+print('Navid'<'Navid1')
+print('Navid'<'navid')
+print('Unicode value for n', ord('n'))
+print('Unicode value for N', ord('N'))
+
+# Type conversion
+my_str='56'
+my_int=int(my_str)
+print(my_str, type(my_str))
+print(my_int, type(my_int))
+print('20'+str(22))
