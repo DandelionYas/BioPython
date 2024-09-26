db20770ccf7f64df6f4f190ddbc30654ef5fe488
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-09-26 09:56:05 +0330

    Chapter 14: Getting started with BioPython

---
+++ BioPython/Chapter-14.py
@@ -0,0 +1,22 @@
+# Upgrade pip:
+# python.exe -m pip install --upgrade pip
+
+# Install Biopython:
+# pip install biopython
+
+import Bio
+# see help of Biopython 
+# help(Bio)
+
+# Exercise  1
+# What is Biopython?
+# Its a set of freely available tools for Biological computation written in python by an international team of developers
+
+# Exercise 2
+# Why we need to learn how to use biopython?
+# We need to learn it because it provides us a lot of ready to use functions for Biological Computing. 
+# This way we will not vast time to build the tools from srcutch
+
+# Exercise 3
+# Is there any alternatives for BioPython?
+# Yes, Pandas and NumPy are the examples of Biopython Alternatives
